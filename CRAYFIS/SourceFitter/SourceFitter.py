3e1ac21ce046a254035fb9f8866020b46a42bce9
dburns7 <dburns@ucdavis.edu>
2014-12-18 16:30:21 -0800

    tweaked source fitter to fit data

--- CRAYFIS/SourceFitter/SourceFitter.py
+++ CRAYFIS/SourceFitter/SourceFitter.py
@@ -2,12 +2,19 @@ import numpy as np
 import scipy.optimize as opt
 import pylab as pl
 
-filein = 'r2.mc'
+filein = 'android.dat'
+#filein = 'r2.mc'
 data = np.loadtxt(open(filein,"r"),delimiter=" ")
 
 x = data[:,0]
-r = data[:,1]
-err_r = data[:,2]
+N = data[:,1]
+t = data[:,2]
+r = data[:,3] 
+err_r = [0, 0, 0, 0, 0]
+for i in range(0, 5):
+  err_r[i] = r[i] * np.sqrt(N[i] + (10/t[i])**2 + 4*(0.25/x[i])**2)
+print err_r
+#err_r = data[:,2]
 
 rmax = r.max()
 r = r/rmax
@@ -29,11 +36,11 @@ print b
 
 fig1 = pl.figure()
 pl.plot(x, r*rmax, color='blue', linewidth='1', label='Data')
-pl.plot(x,a+b*x, color='red', linewidth='1', label='Fit') #test with linear model
-#pl.plot(x, a / (x - b)**2, color='red', linewidth='2', label='Fit')
+#pl.plot(x,a+b*x, color='red', linewidth='1', label='Fit') #test with linear model
+pl.plot(x, a / (x - b)**2, color='red', linewidth='2', label='Fit')
 pl.xlabel('X [in]')
 pl.ylabel('Rate [Candidates/s]')
-pl.legend(loc=2)
+pl.legend(loc=1)
 pl.show()
 fig1.savefig('rate.png')
 
